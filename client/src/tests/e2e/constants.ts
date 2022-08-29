23fcf96504b831e4d35b5a4e8d1f335786417e27
BertrandD <volubyl@pm.me>
2022-08-29 16:18:25 +0200

    update init data

--- client/src/tests/e2e/constants.ts
+++ client/src/tests/e2e/constants.ts
@@ -1,5 +1,5 @@
 export const TEST_EMAIL = "catalogue.demo@yopmail.com";
-export const TEST_PASSWORD = "demo";
+export const TEST_PASSWORD = "password1234";
 export const ADMIN_EMAIL = "admin@catalogue.data.gouv.fr";
 export const STATE_AUTHENTICATED = "./src/tests/e2e/storage/authenticated.json";
 export const STATE_AUTHENTICATED_ADMIN =
