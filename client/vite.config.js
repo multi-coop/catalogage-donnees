9425bedfeb8424e5d30bc3f9f2e7c505f10efe14
Florimond Manca <florimond.manca@fairness.coop>
2022-08-31 18:10:07 +0200

    Purge and minify CSS (#397)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import path from "path";
 import dotenv from "dotenv";
+import purgecss from "@fullhuman/postcss-purgecss";
+import cssnano from "cssnano";
 
 // Vite loads .env contents into process.env for source files.
 // But this is the config file from where it all begins.
@@ -8,6 +10,20 @@ import dotenv from "dotenv";
 dotenv.config({
   path: path.resolve("..", ".env"),
 });
+
+const postcssPlugins = [];
+
+if (process.env.NODE_ENV === "production") {
+  postcssPlugins.push(
+    purgecss({
+      content: ["./src/**/*.html", "./src/**/*.svelte"],
+      safelist: [
+        /svelte-/, // Don't purge custom CSS defined in Svelte component <style> sections
+      ],
+    })
+  );
+  postcssPlugins.push(cssnano());
+}
 
 /**
  * @type {import('vite').UserConfig}
@@ -18,6 +34,11 @@ export const config = {
     alias: {
       src: path.resolve("./src"),
       $lib: path.resolve("./src/lib"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: postcssPlugins,
     },
   },
 };
