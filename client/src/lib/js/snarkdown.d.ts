983bbaa49636cac6996a9eec8f5376be4de89276
florimondmanca <florimond.manca@fairness.coop>
2022-09-08 16:37:48 +0200

    Use snarkdown from npm

--- client/src/lib/js/snarkdown.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "@js/snarkdown.js" {
-  const snarkdown: (text: string) => string;
-  export default snarkdown;
-}
--- client/src/lib/util/markdown.ts
+++ client/src/lib/util/markdown.ts
@@ -1,5 +1,4 @@
-// Source: https://unpkg.com/browse/snarkdown@2.0.0/dist/snarkdown.modern.js
-import snarkdown from "@js/snarkdown.js";
+import snarkdown from "snarkdown";
 
 export const renderMarkdown = (content: string): string => {
   return snarkdown(content);
