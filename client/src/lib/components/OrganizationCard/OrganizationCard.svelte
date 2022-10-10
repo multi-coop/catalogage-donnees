fb6dc69cdc9be31287703d5c0146295941d77d17
DaFrenchFrog <romain.gle@gmail.com>
2022-10-10 14:37:48 +0200

    Ajout des cartes organisation

---
+++ client/src/lib/components/OrganizationCard/OrganizationCard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let name: string;
+  export let src: string;
+  export let status: string;
+</script>
+
+<div class="fr-card">
+  <div class="fr-card__body">
+    <div class="fr-card__content">
+      <h3 class="fr-card__title">
+        {name}
+      </h3>
+      <div class="fr-card__start">
+        {#if status == "catalog"}
+          <p class="fr-badge fr-badge--success">catalogue Activé</p>
+        {:else if status == "registered"}
+          <p class="fr-badge fr-badge--info">Enregistré</p>
+        {:else if status == "pending"}
+          <p class="fr-badge fr-badge--new">En cours</p>
+        {/if}
+      </div>
+    </div>
+  </div>
+  <div class="fr-card__header">
+    <div class="fr-card__img">
+      <img class="fr-responsive-img fr-p-2w" {src} alt="logo {name}" />
+    </div>
+  </div>
+</div>
+
+<style>
+</style>
