587a17c6207c52101799e07083a76fa46b96b2f7
Volubyl <volubyl@pm.me>
2022-11-03 11:54:19 +0100

    add 'declaration accessibilité page' (#525)

---
+++ client/src/routes/(app)/declaration-daccessibilite/+page.svelte
@@ -0,0 +1,121 @@
+<div class="fr-container fr-mt-8w">
+  <div class="fr-grid-row">
+    <div class="fr-col">
+      <h1>Déclaration d’accessibilité</h1>
+
+      <p>Établie le 3 novembre 2022</p>
+
+      <p>
+        Etalab s’engage à rendre son site internet accessibles conformément à
+        l’article 47 de la loi n° 2005-102 du 11 février 2005. À cette fin, il
+        met en œuvre une stratégie et des actions décrite à <a
+          href="https://github.com/etalab/catalogage-donnees/issues/517"
+          target="_blank"
+          rel="noopener"
+        >
+          cette adresse
+        </a>
+        . Cette déclaration d’accessibilité s’applique au service https://catalogue.data.gouv.fr/
+      </p>
+
+      <h2>État de conformité</h2>
+
+      <p>
+        Le site https://catalogue.data.gouv.fr/ est <strong>non conforme</strong
+        >
+        avec le référentiel général d’amélioration de l’accessibilité pour les administrations,
+        RGAA version 4 car il n’existe aucun résultat d’audit en cours de validité
+        permettant de mesurer le respect des critères. Les corrections des non-conformités
+        sont en cours dans
+        <a
+          href="https://github.com/etalab/catalogage-donnees/issues/517"
+          target="_blank"
+          rel="noopener"
+          >un projet de de mise en conformité du site web
+        </a>, et la présente déclaration d’accessibilité sera mise à jour au
+        plus vite.
+      </p>
+
+      <h2>Résultat des tests</h2>
+
+      <p>
+        En l’absence d’audit de conformité il n’y a pas de résultats de tests.
+      </p>
+
+      <h2>Contenus non accessibles</h2>
+
+      <p>
+        <strong>Non conformité</strong><br />
+        En l’absence d’audit tous les contenus seront considérés comme non accessibles
+        par hypothèse.<br /><strong
+          >Dérogations pour charge disproportionnée</strong
+        ><br />
+        En l’absence d’audit aucune dérogation n’a été établie.<br /><strong
+          >Contenus non soumis à l’obligation d’accessibilité</strong
+        ><br />
+        En l’absence d’audit aucun contenu n’a été identifié comme n’entrant pas
+        dans le champ de la législation applicable.<br /><strong
+          >Technologies utilisées pour la réalisation du site «
+          interieur.gouv.fr»</strong
+        ><br />
+        En l'absence d'audit aucune technologie n'a été utilisée.<br /><strong
+          >Agents utilisateurs, technologies d’assistance et outils utilisés
+          pour vérifier l’accessibilité</strong
+        ><br />
+        En l’absence d’audit aucun agent utilisateur et aucune technologie d’assistance
+        n’ont été utilisés.<br /><strong
+          >Les tests des pages web ont été effectués avec les combinaisons de
+          navigateurs web et lecteurs d’écran suivants :</strong
+        ><br />
+        En l’absence d’audit aucune combinaison de navigateur et de lecteur d’écran
+        n’a été utilisée.<br /><strong
+          >Les outils suivants ont été utilisés lors de l’évaluation :</strong
+        ><br />
+        En l’absence d’audit aucun outil n’a été utilisé lors de l’évaluation.<br
+        /><strong
+          >Pages du site ayant fait l’objet de la vérification de conformité</strong
+        ><br />
+        En l’absence d’audit aucune page n’a fait l’objet de la vérification de conformité.
+      </p>
+
+      <h2>Retour d’information et contact</h2>
+      <p>
+        Si vous n’arrivez pas à accéder à un contenu ou à un service, vous
+        pouvez contacter le responsable du site internet pour être orienté vers
+        une alternative accessible ou obtenir le contenu sous une autre forme.
+      </p>
+      <ul>
+        <li>
+          Contacter <a
+            href="mailo:catalogue@data.gouv.fr&subject=[catalogage]: Au sujet de l'accessibilité"
+            >Etalab</a
+          >
+        </li>
+      </ul>
+
+      <h2>Amélioration et contacts</h2>
+
+      <p>
+        Vous avez signalé au responsable du site internet un défaut
+        d’accessibilité qui vous empêche d’accéder à un contenu ou à un des
+        services du portail et vous n’avez pas obtenu de réponse satisfaisante.<br
+        />
+        Écrire un message au
+        <a
+          href="https://formulaire.defenseurdesdroits.fr/"
+          rel="noopener noreferrer"
+          target="_blank">Défenseur des droits</a
+        > <br />
+        Contacter le délégué du
+        <a
+          href="https://www.defenseurdesdroits.fr/saisir/delegues"
+          rel="noopener noreferrer"
+          target="_blank">Défenseur des droits dans votre région</a
+        > <br />
+        Envoyer un courrier par la poste (gratuit, ne pas mettre de timbre) Défenseur
+        des droits<br />
+        Libre réponse 71120 75342 Paris CEDEX 07
+      </p>
+    </div>
+  </div>
+</div>
---
+++ client/src/routes/(app)/declaration-daccessibilite/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from "./$types";
+import { SITE_TITLE } from "src/constants";
+
+export const prerender = true;
+export const ssr = true;
+
+export const load: PageLoad = () => {
+  return {
+    title: `Déclaration accessibilité - ${SITE_TITLE}`,
+  };
+};
