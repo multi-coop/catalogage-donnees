931d2fb91b696dcef0a82dc580b495039e812e78
volubyl <volubyl@pm.me>
2022-11-24 17:48:39 +0100

    fix: playwright config

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -25,7 +25,7 @@ const config: PlaywrightTestConfig<AppTestArgs> = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html"], ["list"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "http://localhost:3000",
--- client/src/tests/e2e/a11y.spec.ts
+++ client/src/tests/e2e/a11y.spec.ts
@@ -1,21 +1,16 @@
 
 
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright'; // 1
 
 
 test.describe('homepage', () => { // 2
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto('/'); // 3
-
-    // try {
-      //  const axeBuilder = new  AxeBuilder({page})
-
-      //  const results = await axeBuilder.analyze();
-
-      //  console.log(results);
-     
-    //   } catch (e) {
-    //      console.log(e)
-    //   }
+        await page.goto('/'); // 3
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore this is a hot fix untild this isssue will be resolved https://github.com/dequelabs/axe-core-npm/issues/601
+        const accessibilityScanResults = await new AxeBuilder.default({ page }).analyze(); // 4
+    
+        expect(accessibilityScanResults.violations).toEqual([]); // 5
   });
 });
