7fa9f4b5f2122b298ed641dab016e13263dd6761
volubyl <volubyl@pm.me>
2022-12-01 14:09:13 +0100

    WIP

--- tests/tools/test_remove_duplicated_tags.py
+++ tests/tools/test_remove_duplicated_tags.py
@@ -1,17 +1,32 @@
 import pytest
 
 from server.application.catalogs.commands import CreateCatalog
+from server.application.datasets.queries import GetDatasetByID
 from server.application.tags.queries import GetAllTags
 from server.config.di import resolve
 from server.domain.common.types import Skip
+from server.domain.datasets.entities import Dataset
 from server.seedwork.application.messages import MessageBus
-from tools.remove_duplicated_tags import main
+from tools.remove_duplicated_tags import link_dataset_with_tags_to_keep, main
 
 from ..factories import (
     CreateDatasetFactory,
     CreateOrganizationFactory,
     CreateTagFactory,
 )
+
+
+def test_link_dataset_with_tags_to_keep() -> None:
+
+    tag_1 = CreateTagFactory.build(name="tag-1", id="1234")
+    tag2 = CreateTagFactory.build(name="tag-2", id="5678")
+    tag2 = CreateTagFactory.build(name="tag-1", id="9134")
+
+    dataset = CreateDatasetFactory.build(account=Skip(), tags=["5678", "9134"])
+
+    table_of_truth = {"tag-1": "9134", "tag-2": "1234"}
+
+    assert 2 == 1
 
 
 @pytest.mark.asyncio
@@ -35,7 +50,7 @@ async def test_remove_duplicated_tags() -> None:
     tag_id_2 = await bus.execute(CreateTagFactory.build(name=tag_name))
     tag_id_3 = await bus.execute(CreateTagFactory.build(name="not_dulicated_tag"))
 
-    await bus.execute(
+    dataset_id_1 = await bus.execute(
         CreateDatasetFactory.build(
             account=Skip(),
             tag_ids=[tag_id_1, tag_id_2, tag_id_3],
@@ -43,8 +58,22 @@ async def test_remove_duplicated_tags() -> None:
         )
     )
 
+    dataset_id_2 = await bus.execute(
+        CreateDatasetFactory.build(
+            account=Skip(),
+            tag_ids=[tag_id_2],
+            organization_siret=siret,
+        )
+    )
+
     await main()
 
     tags = await bus.execute(GetAllTags())
-
     assert len(tags) == 2
+
+    dataset_1 = await bus.execute(GetDatasetByID(id=dataset_id_1))
+    assert len(dataset_1.tags) == 2
+
+    dataset_2 = await bus.execute(GetDatasetByID(id=dataset_id_2))
+    assert len(dataset_2.tags) == 1
+    assert dataset_2.tags[0].id == tag_id_1
--- tools/remove_duplicated_tags.py
+++ tools/remove_duplicated_tags.py
@@ -1,7 +1,10 @@
 import asyncio
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from server.config.di import bootstrap, resolve
+from server.domain.common.types import ID
+from server.domain.datasets.entities import Dataset
+from server.domain.datasets.repositories import DatasetGetAllExtras, DatasetRepository
 from server.domain.tags.entities import Tag
 from server.domain.tags.repositories import TagRepository
 from server.seedwork.application.messages import MessageBus
@@ -20,21 +23,73 @@ def group_tag_by_name(tags: List[Tag]) -> Dict[str, List[str]]:
     return groupped_by_tag_by_name
 
 
+def build_tag_table_of_truth(
+    dataset_results: List[Tuple[Dataset, DatasetGetAllExtras]]
+) -> Dict[str, ID]:
+
+    tag_table_of_truth: Dict[str, ID] = {}
+
+    for dataset, _ in dataset_results:
+        tags = dataset.tags
+        for tag in tags:
+            if tag.name not in tag_table_of_truth:
+                tag_table_of_truth[tag.name] = tag.id
+
+    return tag_table_of_truth
+
+
+def link_dataset_with_tags_to_keep(
+    dataset_results: List[Tuple[Dataset, DatasetGetAllExtras]],
+    table_of_truth: Dict[str, ID],
+) -> Dict[ID, Tuple[Dataset, List[str]]]:
+    for dataset, _ in dataset_results:
+        pass
+
+
 async def main() -> None:
     bus = resolve(MessageBus)
     tag_repository = resolve(TagRepository)
+    datasets_repository = resolve(DatasetRepository)
 
     tags = await tag_repository.get_all()
+    results = await datasets_repository.get_all()
 
     groupped_by_tag_by_name = group_tag_by_name(tags)
 
-    duplicated_ids: List[str] = []
+    # Table of truth
+    #  name => id
+    #
+    #
+    #
 
-    for key, value in groupped_by_tag_by_name.items():
-        if len(value) > 1:
-            duplicated_ids = duplicated_ids + value[1:]
+    # Création de la table de vérite
+    # Pour chaque datasets
+    #   Récupérer les tags du dataset
+    #   Pour chaque tag du dataset
+    #       Est-ce que le tag est dans la table de vérité
+    #           Non, on ajoute le tag dans la table de vériTé
+    #           Oui
+    #               Est-ce que l'id du tag correspond à l'id de la table de vérité
+    #                   Oui, on ajoute l'id du tag dans le tableau des tags à garder pour le dataset
+    #                   Non, on passe au suivant
+    #
+    #
+    #
 
-    await tag_repository.delete_many_by_id(duplicated_ids)
+    tag_table_of_truth: Dict[str, ID] = {}
+    tags_to_keep: Dict[ID, Tuple[Dataset, List[str]]] = {}
+
+    dataset_results = results[0]
+
+    tag_table_of_truth = build_tag_table_of_truth(dataset_results)
+
+    breakpoint()
+
+    # for key, value in groupped_by_tag_by_name.items():
+    #     if len(value) > 1:
+    #         duplicated_ids = duplicated_ids + value[1:]
+
+    # await tag_repository.delete_many_by_id(duplicated_ids)
 
 
 if __name__ == "__main__":
