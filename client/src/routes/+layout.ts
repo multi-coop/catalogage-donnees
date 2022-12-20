901efc0624c48300d00ce97bc68a5f3c33d1dfe0
volubyl <volubyl@pm.me>
2022-12-20 16:56:16 +0100

    fix: not reachable 404 page

--- client/src/routes/+layout.ts
+++ client/src/routes/+layout.ts
@@ -1,6 +1,3 @@
-import type { LayoutLoad } from "./$types";
-import { authGuard } from "src/lib/util/auth";
-
 // NOTE: SSR usage is aimed at improving SEO, but most of our pages are
 // hidden behind authentication.
 // SSR will be enabled selectively on appropriate pages.
@@ -8,4 +5,3 @@ import { authGuard } from "src/lib/util/auth";
 // for private pages can then be exclusively managed in the browser.
 // See: https://github.com/etalab/catalogage-donnees/pull/143
 export const ssr = false;
-export const load: LayoutLoad = authGuard;
