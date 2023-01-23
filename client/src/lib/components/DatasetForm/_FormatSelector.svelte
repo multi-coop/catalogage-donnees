6551df2e6cc9e9d41c4dc0856cc824a97c0e1dce
volubyl <volubyl@pm.me>
2023-01-23 14:43:55 +0100

    feat: component keyboard interractions

--- client/src/lib/components/DatasetForm/_FormatSelector.svelte
+++ client/src/lib/components/DatasetForm/_FormatSelector.svelte
@@ -3,19 +3,29 @@
   import RequiredMarker from "../RequiredMarker/RequiredMarker.svelte";
   import { escape } from "src/lib/util/string";
 
-  export let value: string | null = null;
   export let name: string;
   export let error = "";
 
-  let currentLiIndex = 0;
-
   let suggestionList: HTMLElement;
-  let previousNode: HTMLElement;
+
+  let value: string | null = null;
+  let currentLiIndex: number = 0;
+  let showSuggestions = false;
+  let selectedOption: SelectOption<number> | undefined;
+
+  let textBoxHasFocus = false;
+
+  $: console.log({ value, showSuggestions, textBoxHasFocus, currentLiIndex });
 
   $: regexp = value ? new RegExp(escape(value), "i") : null;
+
   $: filteredSuggestions = showSuggestions
     ? suggestions.filter((item) => (regexp ? item.label.match(regexp) : true))
     : [];
+
+  $: activeSuggestionDescendant = currentLiIndex
+    ? `suggestion-item-${currentLiIndex}`
+    : null;
 
   export let suggestions: SelectOption<number>[] = [
     {
@@ -30,51 +40,201 @@
       label: "titi",
       value: 3,
     },
+    {
+      label: "fruiti",
+      value: 4,
+    },
+    {
+      label: "tarte",
+      value: 5,
+    },
   ];
 
   let setSelectedOption = (value: string): SelectOption<number> | undefined => {
-    return suggestions.find((item) => item.label === value);
+    return filteredSuggestions.find((item) => item.label === value.trim());
   };
-  let showSuggestions = false;
+
+  const handleClickOption = (optionValue: string) => {
+    selectedOption = setSelectedOption(optionValue);
+
+    if (selectedOption) {
+      value = selectedOption.label;
+    }
+    showSuggestions = false;
+  };
 
   const handleInput = (ev: Event & { currentTarget: HTMLInputElement }) => {
     value = ev.currentTarget.value;
   };
 
-  const handleKeyboard = (e: KeyboardEvent) => {
+  const manageKeyboardInterractions = (e: KeyboardEvent) => {
     let suggestionItems = suggestionList.childNodes;
 
-    console.log("BEFORE:", {
-      currentLiIndex,
-      itemLenght: suggestionItems.length,
-    });
+    if (e.altKey && e.key === "ArrowDown") {
+      // Opens the listbox without moving focus or changing selection.
+      showSuggestions = true;
+      textBoxHasFocus = false;
+      return;
+    }
 
-    switch (e.key) {
-      case "Enter":
-        showSuggestions = !showSuggestions;
+    // Manage keyboard interracions when textbox has focus
+    if (textBoxHasFocus) {
+      switch (e.key) {
+        case "Enter":
+          //  Closes the listbox if it is displayed.
 
-        break;
+          if (showSuggestions) {
+            textBoxHasFocus = false;
+            showSuggestions = false;
+          }
 
-      case "Escape":
-        showSuggestions = false;
-        break;
+          break;
 
-      case "ArrowDown":
-        currentLiIndex = currentLiIndex + 1;
-        if (currentLiIndex === suggestionItems.length) {
-          currentLiIndex = 0;
-        }
-        break;
+        case "Escape":
+          //If the listbox is displayed, closes it.
 
-      case "ArrowUp":
-        if (currentLiIndex === 0) {
-          currentLiIndex = suggestionItems.length - 1;
-        } else {
-          currentLiIndex = currentLiIndex - 1;
-        }
+          if (showSuggestions) {
+            textBoxHasFocus = true;
+            showSuggestions = false;
+            return;
+          }
 
-      default:
-        break;
+          // if the listbox is not displayed, clears the textbox.
+
+          if (!showSuggestions) {
+            textBoxHasFocus = true;
+            value = "";
+          }
+          break;
+
+        case "ArrowDown":
+          // If the textbox is not empty and the listbox is displayed, moves visual focus to the first suggested value.
+          textBoxHasFocus = false;
+
+          if (value && showSuggestions) {
+            currentLiIndex = 0;
+          }
+
+          // the textbox is empty and the listbox is not displayed, opens the listbox and moves visual focus to the first option.
+
+          if (!value && !showSuggestions) {
+            currentLiIndex = 0;
+            showSuggestions = true;
+          }
+
+          break;
+
+        case "ArrowUp":
+          // console.log("tata");
+          // if the textbox is not empty and the listbox is displayed, moves visual focus to the last suggested value.
+
+          if (!value && showSuggestions) {
+            currentLiIndex = filteredSuggestions.length;
+          }
+
+          // If the textbox is empty, first opens the listbox if it is not already displayed and then moves visual focus to the last option.
+
+          if (!value && !showSuggestions) {
+            textBoxHasFocus = false;
+            showSuggestions = true;
+            currentLiIndex = suggestions.length - 1;
+          }
+
+        default:
+          break;
+      }
+
+      return;
+    }
+    // Manage keyboard interracions when listbox has focus
+
+    if (!textBoxHasFocus) {
+      switch (e.key) {
+        case "Enter":
+          /**
+         * 
+          Sets the textbox value to the content of the focused option in the listbox.
+          Closes the listbox.
+          Sets visual focus on the textbox.
+
+        */
+          if (!currentLiIndex) {
+            return;
+          }
+          const selectedSuggestionItem =
+            suggestionItems[currentLiIndex].textContent;
+
+          if (showSuggestions && selectedSuggestionItem) {
+            selectedOption = setSelectedOption(selectedSuggestionItem);
+
+            if (selectedOption) {
+              value = selectedOption.label;
+              showSuggestions = false;
+            }
+          }
+
+          textBoxHasFocus = true;
+          break;
+
+        case "Escape":
+          /**
+         * 
+
+            Closes the listbox.
+            Sets visual focus on the textbox.
+
+        */
+
+          showSuggestions = false;
+          textBoxHasFocus = true;
+          break;
+
+        /**
+           * 
+              Moves visual focus to the next option.
+              If visual focus is on the last option, moves visual focus to the first option.
+              Note: This wrapping behavior is useful when Home and End move the editing cursor as described below.
+
+          */
+
+        case "ArrowDown":
+          currentLiIndex += 1;
+
+          if (currentLiIndex === suggestionItems.length) {
+            currentLiIndex = 0;
+          }
+          break;
+
+        case "ArrowUp":
+          /**
+           * 
+              Moves visual focus to the previous option.
+              If visual focus is on the first option, moves visual focus to the last option.
+              Note: This wrapping behavior is useful when Home and End move the editing cursor as described below.
+
+          */
+          if (currentLiIndex === 0) {
+            currentLiIndex = suggestionItems.length - 1;
+          } else {
+            currentLiIndex -= 1;
+          }
+          break;
+        case "ArrowRight":
+          /**
+           * 
+            Moves visual focus to the textbox and moves the editing cursor one character to the right.
+
+          */
+          textBoxHasFocus = true;
+          break;
+        case "ArrowLeft":
+          /**
+           *  	Moves visual focus to the textbox and moves the editing cursor one character to the left.
+           */
+          textBoxHasFocus = true;
+        default:
+          break;
+      }
     }
   };
 </script>
@@ -93,11 +253,12 @@
   </label>
 
   <input
-    on:keydown={handleKeyboard}
+    on:keydown={manageKeyboardInterractions}
     class="fr-input"
     class:fr-input--error={error}
+    class:inputOutline={textBoxHasFocus}
     type="text"
-    name="geographicalCoverage"
+    {name}
     id={name}
     {value}
     required
@@ -107,9 +268,10 @@
     aria-autocomplete="list"
     aria-expanded={showSuggestions}
     aria-describedby={error ? "geographicalCoverage-desc-error" : null}
+    aria-activedescendant={activeSuggestionDescendant}
     on:input={handleInput}
-    on:focus={() => (showSuggestions = true)}
-    on:focusout={() => (showSuggestions = false)}
+    on:focus={() => (textBoxHasFocus = true)}
+    on:focusout={() => (textBoxHasFocus = false)}
   />
 
   <ul
@@ -118,12 +280,16 @@
     id={`${name}-suggestions`}
     role="listbox"
     aria-label="Formats de données"
+    class:focused={!textBoxHasFocus}
   >
-    {#each filteredSuggestions as { label, value }, index}
+    {#each filteredSuggestions as { label }, index}
       <li
         class:focused={index === currentLiIndex}
-        id={value.toString()}
+        id={`suggestion-item-${index}`}
         role="option"
+        aria-label={name}
+        aria-selected={index === currentLiIndex}
+        on:click={() => handleClickOption(label)}
       >
         {label}
       </li>
@@ -137,6 +303,15 @@
   }
 
   .focused {
-    background-color: bisque;
+    border: 1px solid saddlebrown;
+  }
+
+  input:focus-visible {
+    /* Remove the focus visible default behavior. Will be managed programatticaly  */
+    outline-style: none;
+  }
+
+  .inputOutline {
+    outline-style: solid !important;
   }
 </style>
